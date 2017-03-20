dac86541c2629d047eaa06b48e29e19fffbc98fb
YaJunCui <644583255@qq.com>
2017-03-20 14:13:59 +0800

    modify

--- C++/Effective Modern CPP/第一章 类型推导/01_code.cpp
+++ C++/Effective Modern CPP/第一章 类型推导/01_code.cpp
@@ -1,23 +1,86 @@
 #include <iostream>
 #include <typeinfo>
+#include <cstdlib>
+
+#ifndef _MSC_VER
+#include <cxxabi.h>
+#endif
 
 using namespace std;
 
+// template <typename T>
+// void f(const T& param)
+// {
+//   cout << typeid(T).name() << endl;
+//   cout << param << endl;
+// }
+
 template <typename T>
-void f(const T& param)
+void f0(T& param)
 {
-  cout << typeid(T).name() << endl;
-  cout << param << endl;
+   char* name = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr);
+   cout << name << endl;
+   free(name);
+}
+
+template <typename T>
+void f1(const T& param)
+{
+   char* name = abi::__cxa_demangle(typeid(param).name(), nullptr, nullptr, nullptr);
+   cout << __FUNCTION__ << " " << name << endl;
+   free(name);
+}
+
+template<typename T>
+void f2(T* param)
+{
+   char* name = abi::__cxa_demangle(typeid(param).name(), nullptr, nullptr, nullptr);
+   cout << __FUNCTION__ << " " << name << endl;
+   free(name);
+}
+
+template<typename T>
+void f3(const T* param)
+{
+   char* name = abi::__cxa_demangle(typeid(param).name(), nullptr, nullptr, nullptr);
+   cout << __FUNCTION__ << " " << name << endl;
+   free(name);
 }
 
 
+template<typename T>
+void f4(T&& param)
+{
+  char* name = abi::__cxa_demangle(typeid(param).name(), nullptr, nullptr, nullptr);
+  cout << __FUNCTION__ << " " << name << endl;
+  free(name);
+}
+
 int main()
 {
-  f(10);
+  // int x = 27;
+  // f(x);
+  // const int cx = x;
+  // f(cx);
+  // const int& rx = x;
+  // f1(rx);
 
-  int x = 0;
+  // int x = 27;
+  // const int *px = &x;
+  // const int * const px1 = px;
 
-  f(x);
+  // f3(&x);  //T是int，        param的类型是int*
+  // f3(px);  //T是cosnt int，  param的类型是const int*
+  // f3(px1);  //T是cosnt int，  param的类型是const int*
+
+  int x = 27;
+  const int cx = x;
+  const int& rx = x;
+
+  f4(x);   
+  f4(cx);  
+  f4(rx);  
+  f4(27);  
 
   return 0;
 }
